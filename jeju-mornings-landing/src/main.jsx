8a2014ab05267df545b1c7382af6c5d66790ff0d
soulution <nmjoon@gmail.com>
2025-09-22 15:11:19 +0900

    Update main.jsx

--- jeju-mornings-landing/src/main.jsx
+++ jeju-mornings-landing/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css' // Tailwind CSS 스타일을 불러옵니다.
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 )
