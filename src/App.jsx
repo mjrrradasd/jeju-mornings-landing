de61f659ef0a6b0b2502e54c5aba9ed28f43bc99
soulution <nmjoon@gmail.com>
2025-09-22 10:45:29 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -101,7 +101,7 @@ export default function JejuMorningsLandingPage() {
           </div>
           <div>
             <img
-              src="/mockup-image.png"
+              src="/mockup image.png"
               alt="Jeju Mornings Calm Focus Gummies"
               className="rounded-2xl shadow-2xl"
             />
@@ -309,7 +309,7 @@ export default function JejuMorningsLandingPage() {
       <section id="shop" className="py-24 bg-white">
         <div className="max-w-4xl mx-auto px-6 text-center">
           <img
-            src="/mockup-image.png"
+            src="/mockup image.png"
             alt="Jeju Mornings Calm Focus Gummies"
             className="mx-auto mb-8 w-64 rounded-xl"
           />
